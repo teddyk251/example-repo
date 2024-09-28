4e8fb1e2f072b883991a6f01a70dedf1eaeedfb4
Alexgichamba <alexgichamba@ieee.org>
2024-09-28 09:30:04 +0200

    add amazon support

--- translate/translate.py
+++ translate/translate.py
@@ -1,13 +1,30 @@
+import boto3
 from google.cloud import translate
 from google.api_core.exceptions import GoogleAPIError
+import os
 
-def translate_text(text: str = "Kwa hivyo", project_id: str = "idl-s24", source_lang: str = "sw", target_lang: str = "en") -> translate.TranslationServiceClient:
-    """Translating Text."""
+# get AWS_REGION from environment variable
+AWS_REGION = os.getenv("AWS_REGION")
 
+def translate_text(text: str = "Kwa hivyo", 
+                   project_id: str = "idl-s24", 
+                   source_lang: str = "sw", 
+                   target_lang: str = "en", 
+                   service: str = "google") -> str:
+    """Translates text using either Google or Amazon Translate."""
+    
+    if service == "google":
+        return google_translate(text, project_id, source_lang, target_lang)
+    elif service == "amazon":
+        return amazon_translate(text, source_lang, target_lang, region=AWS_REGION)
+    else:
+        raise ValueError("Unsupported service. Choose either 'google' or 'amazon'.")
+
+def google_translate(text: str, project_id: str, source_lang: str, target_lang: str) -> str:
+    """Uses Google Cloud Translate to translate text."""
+    
     client = translate.TranslationServiceClient()
-
     location = "global"
-
     parent = f"projects/{project_id}/locations/{location}"
 
     try:
@@ -20,9 +37,24 @@ def translate_text(text: str = "Kwa hivyo", project_id: str = "idl-s24", source_
                 "target_language_code": target_lang,
             }
         )
-
         # Return the first translated text
         return response.translations[0].translated_text if response.translations else None
     except GoogleAPIError as e:
-        print(f"Error translating text: {e}")
+        print(f"Error translating text with Google: {e}")
+        return None
+
+def amazon_translate(text: str, source_lang: str, target_lang: str, region: str) -> str:
+    """Uses Amazon Translate to translate text."""
+    
+    client = boto3.client('translate', region_name=AWS_REGION)
+    
+    try:
+        response = client.translate_text(
+            Text=text,
+            SourceLanguageCode=source_lang,
+            TargetLanguageCode=target_lang
+        )
+        return response['TranslatedText']
+    except Exception as e:
+        print(f"Error translating text with Amazon: {e}")
         return None
