4c363d9beee3089357ff8146fde1f3a29f61f4b8
Tewodros Kederalah Idris <34500735+teddyk251@users.noreply.github.com>
2024-09-28 13:03:44 +0200

    refactor modules

--- speech/test_stt.py
+++
@@ -1,4 +0,0 @@
-from stt import transcribe_audio
-
-transcription = transcribe_audio("speech/output_converted.wav", "rw")
-print(f"Transcription: {transcription}")
--- speech/test_tts.py
+++
@@ -1,3 +0,0 @@
-from tts import synthesize_text_to_speech
-
-audio_file = synthesize_text_to_speech("Mbona unakaa hivi?", "sw")
